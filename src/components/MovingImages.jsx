6b9ba5c79fe2270270159282f563b80e05e384d3
saittd03 <saittd03@gmail.com>
2025-04-16 11:54:39 +0530

    Update MovingImages.jsx

--- src/components/MovingImages.jsx
+++ src/components/MovingImages.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./MovingImages.css";
 
 const imageUrls = [
-  "https://yourdomain.com/image1.jpg",
-  "https://yourdomain.com/image2.jpg",
-  "https://yourdomain.com/image3.jpg",
-  "https://yourdomain.com/image4.jpg",
-  "https://yourdomain.com/image5.jpg",
-  "https://yourdomain.com/image6.jpg",
-  "https://yourdomain.com/image7.jpg",
+  "https://tirumalatirupatiyatra.in/wp-content/uploads/2024/07/Malayappa-Swamy-Saalakatla-Brahmotsavams-and-Navaratri-Brahmotsavams.jpg",
+  "https://i.pinimg.com/736x/7a/33/f3/7a33f36196448a5d160327689537e6b8.jpg",
+  "https://icctmemphis.org/wp-content/uploads/2024/10/Sesha_Vahanam.jpg",
+  "https://templesofsouthindia.wordpress.com/wp-content/uploads/2013/06/kalpavriksha-vahanam-2012.jpg?w=584",
+  "https://icctmemphis.org/wp-content/uploads/2024/10/Hanumath_Vahanam.jpg",
+  "https://icctmemphis.org/wp-content/uploads/2024/10/Hamsa_Vahanam.jpg",
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhNgKqvpn36ThYhADs5KSfqstQnH7RhQURPFUXp8GaaAFJjDqiFpGjPLsSFw4CLUrjy9yEQLdzQBT1jK0IvqXLFgL6xVgmh0XqE4_0Jg43t5yDaaYTKBg2WnZlZRBfTGRGzoOJvmUXwe58/s1280/119640793_1236250396726553_2163829448560762620_n.jpg",
 ];
 
 const MovingImages = () => {
