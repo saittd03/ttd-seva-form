8a4499f9abc9632fe09334ec78e7e61812aa93a9
saittd03 <saittd03@gmail.com>
2025-04-16 11:43:14 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div style={{ textAlign: "center", padding: "10px" }}>
       <h1 style={{ margin: "10px", color: "brown" }}>Tirumala Tirupati Devasthanams</h1>
-      <img src="https://yourdomain.com/top-image.jpg" alt="TTD Temple" style={{ height: "100px" }} />
+      <img src="https://www.ttdsevaonline.net/wp-content/uploads/2018/02/ttd-tirumala-srivari-seva-services.png" alt="TTD Temple" style={{ height: "100px" }} />
     </div>
   );
 };
