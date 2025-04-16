d56f7a1225bb4173328b328f6945703ebc8837e1
saittd03 <saittd03@gmail.com>
2025-04-16 11:44:55 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ backgroundImage: "url('https://yourdomain.com/background1.jpg')", backgroundSize: "cover", padding: "20px" }}>
+    <div style={{ backgroundImage: "url('https://education.sakshi.com/sites/default/files/2022-05/ttd.jpg')", backgroundSize: "cover", padding: "20px" }}>
       <MovingImages />
       <div style={{ textAlign: "center", marginTop: "50px" }}>
         <button onClick={() => navigate("/form")} style={{ padding: "15px 30px", fontSize: "18px", backgroundColor: "brown", color: "white", border: "none", borderRadius: "10px" }}>
