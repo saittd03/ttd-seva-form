b86404b48b05b190b0ddfa8b188a8bb2f8d2ea4f
root <root@ip-172-31-8-58.ap-south-1.compute.internal>
2025-04-16 07:18:05 +0000

    seva

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   return (
     <div style={{ backgroundImage: "url('https://education.sakshi.com/sites/default/files/2022-05/ttd.jpg')", backgroundSize: "cover", padding: "20px" }}>
       <MovingImages />
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <div style={{ textAlign: "center", marginTop: "100px" }}>
         <button onClick={() => navigate("/form")} style={{ padding: "15px 30px", fontSize: "18px", backgroundColor: "brown", color: "white", border: "none", borderRadius: "10px" }}>
           Add Sevak Details
         </button>
